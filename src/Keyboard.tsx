94db386b6962dcf5eaab59169241dc8fa82ac67e
Izet Delibasic <izetdelibasic55@gmail.com>
2023-10-20 14:18:58 +0200

    Adding style and logic for keyboard buttons

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -1,4 +1,5 @@
-import styles from "./Keyboard.css?inline";
+import styles from "./Keyboard.module.css"
+
 
 
 const KEYS = [
@@ -30,19 +31,31 @@ const KEYS = [
     "z",
   ]
 
-export function Keyboard() {
+  type KeyboardProps = {
+    activeLetters: string[]
+    inactiveLetters: string[]
+    addGuessedLetter: (letter: string) => void
+  }
+
+export function Keyboard({activeLetters, inactiveLetters, addGuessedLetter} : KeyboardProps) {
     return <div style={{display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr)",
     gap: ".5rem",
         }}
     >
         {KEYS.map(key => {
+            const isActive = activeLetters.includes(key)
+            const isInactive = inactiveLetters.includes(key)
             return (
-            <button
-            disabled 
-            className={ `${styles.btn}`}
-            key={key}>
+                <button
+                onClick={() => addGuessedLetter(key)}
+                className={`${styles.btn} ${isActive ? styles.active : ""} ${
+                  isInactive ? styles.inactive : ""
+                }`}
+                disabled={isInactive || isActive}
+                key={key}
+              >
                 {key}
-            </button>
+              </button>
             )
         })}
 
